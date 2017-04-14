bcfc57611fe951a3952d99b340ef73b9de01dd05
anatolii <a.s.ovrutskyi@gmail.com>
2017-04-14 13:51:38 +0300

    lesson 6

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// var i = 100; 
+// while (i > 0) {
+// i = i/2;
+// if (i%2 != 0) {
+// 	continue;
+// }
+// document.write(i + '<br/>');
+// }s
+
+
+// ДЗ 2
+
+for (var i = 1; i <= 9; i++) {
+	for (var j = 1; j <= 9; j++)
+	}
+	i = i*j;
+	document.write(i);
+}
